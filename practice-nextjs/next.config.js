bbf9384eb048133b492661fa7497233ba53cc95c
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-08 23:05:38 +0600

    Allow: Any hostname for external images

--- practice-nextjs/next.config.js
+++ practice-nextjs/next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 
+// here manually i need to config every hostname 
+// module.exports = {
+//   images: {
+//     domains: ['bs-uploads.toptal.io'],
+//   },
+// }
+
+// here I allow all hostname
 module.exports = {
   images: {
-    domains: ['bs-uploads.toptal.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 }
