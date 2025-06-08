b7b0a09f7e438fbbaf804ad52def5d4398386d75
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-09 01:03:18 +0600

    Create: Server side rendaring components

--- practice-nextjs/src/app/shoes/page.jsx
+++ practice-nextjs/src/app/shoes/page.jsx
@@ -1,11 +1,18 @@
 import Card from "@/components/card/page";
 
+export const metadata = {
+  title:"Shoes",
+  description:"Here all shows are shown"
+}
+
+
 const ShoesPage = async () => {
   const res = await fetch("http://localhost:5000/shoes", {
-    // cache:"force-cache"
-    next:{
-      revalidate:5 // 5 means after every 5 sec site will be updated
-    }
+    // cache:"force-cache" // it make static
+    // next:{
+    //   revalidate:5 // 5 means after every 5 sec site will be updated
+    // }
+    cache:'no-store' // its make server side rendaring when client request then just show data don't store 
   });
   const shoes = await res.json();
 
